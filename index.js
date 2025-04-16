bc9d753ad70d3204daf29e298be1c2e0a27463a1
dieudonne-ui <adrienc836@gmail.com>
2025-04-16 18:29:20 +0100

    [ADD] documentation avec swagger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const setupSwagger = require ('./swagger');
+setupSwagger(app);
 
 
 dotenv.config();
---
+++ swagger.js
@@ -0,0 +1,36 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+definition: {
+openapi: '3.0.0',
+info: {
+title: 'ArtisanMap API',
+version: '1.0.0',
+description: 'Documentation de l’API ArtisanMap',
+},
+servers: [
+{
+url: 'https://artisanmap.onrender.com',
+},
+],
+components: {
+securitySchemes: {
+bearerAuth: {
+type: 'http',
+scheme: 'bearer',
+bearerFormat: 'JWT',
+},
+},
+},
+},
+apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
